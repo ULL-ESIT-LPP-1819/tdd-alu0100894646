c10dad5b68fed5c49966779ee06e457563640d08
José F Rodríguez Hernández <alu0100894646@ull.edu.es>
2018-11-06 18:03:54 +0000

    añadida la prueba para comprobar que tiene un nombre

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -1,9 +1,18 @@
+#require 'lib/valor_energetico'
+
 RSpec.describe Tdd do
+  
+  #prueba1 = ValorEnergetico.new("Magdalena")
+  
   it "has a version number" do
     expect(Tdd::VERSION).not_to be nil
   end
 
   it "does something useful" do
     expect(true).to eq(true)
+  end
+  
+  it "Tiene un nombre de etiqueta" do
+    expect("Magdalena").to eq(prueba1.valor)
   end
 end
