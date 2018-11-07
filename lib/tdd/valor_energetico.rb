4fea27b32fe50392d40b2459548f7f825c47f9c5
José F Rodríguez Hernández <alu0100894646@ull.edu.es>
2018-11-07 17:28:50 +0000

    Añadido el código para la cantidad de azúcar

--- lib/tdd/valor_energetico.rb
+++ lib/tdd/valor_energetico.rb
@@ -1,14 +1,14 @@
 class ValorEnergetico
 
-    attr_reader :etiqueta,:grasa,:saturada,:hidratos
+    attr_reader :etiqueta,:grasa,:saturada,:hidratos,:azucar
     
-    def initialize(etiqueta,grasa,saturada,hidratos)
+    def initialize(etiqueta,grasa,saturada,hidratos,azucar)
         
         @etiqueta = etiqueta
         @grasa = grasa
         @saturada = saturada
         @hidratos=hidratos
-        
+        @azucar=azucar
     end
     
 end
