c0f7da2a645bec8078bcd180800d8583641e062d
José F Rodríguez Hernández <alu0100894646@ull.edu.es>
2018-11-15 16:51:44 +0000

    superada la prueba para insertar por el head

--- lib/tdd/dll_etiqueta.rb
+++ lib/tdd/dll_etiqueta.rb
@@ -9,5 +9,19 @@ class Dll
         @tail = nil
     end
     
+    def insert_head(value)
+       
+       nodo = Node.new(value,nil,nil)
+       
+       if(@head == nil)
+           @head = nodo
+           @tail = nodo
+       else
+           @head.prev = nodo
+           nodo.next = @head
+           @head = nodo
+       end
+        
+    end
     
 end
