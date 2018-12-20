751465f168857ac9b427a1b15ee1fff5bb703589
José F Rodríguez Hernández <alu0100894646@ull.edu.es>
2018-12-20 22:16:53 +0000

    Fixing travis

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -2,8 +2,6 @@
 
 RSpec.describe Tdd do
   
-  
-  
   it "has a version number" do
     expect(Tdd::VERSION).not_to be nil
   end
@@ -12,43 +10,46 @@ RSpec.describe Tdd do
     expect(true).to eq(true)
   end
 
-describe ValorEnergetico do
-  
-  prueba1 = ValorEnergetico.new("Nutella",31.6,11.0,57.6,56.8,6.0,0.11,25.0,8)
-  
-  
-  context "Características de los alimentos" do 
+  describe ValorEnergetico do
     
-    it "Tiene un nombre de etiqueta" do
-      expect("Nutella").to eq(prueba1.etiqueta)
-    end
+      
     
-    it "Tiene un valor para las grasas " do 
-      expect(31.6).to eq(prueba1.grasa)
-    end
+    context "Características de los alimentos" do 
+      
+      prueba1 = ValorEnergetico.new("Nutella",31.6,11.0,57.6,56.8,6.0,0.11,25.0,8)
+      
+      it "Tiene un nombre de etiqueta" do
+        expect("Nutella").to eq(prueba1.etiqueta)
+      end
     
-    it "Tiene un valor para las grasas saturadas" do
-      expect(11.0).to eq(prueba1.saturada)
-    end
+      it "Tiene un valor para las grasas " do 
+        expect(31.6).to eq(prueba1.grasa)
+      end
     
-    it "Tiene un valor para la cantidad de hidratos de carbono" do
-      expect(57.6).to eq(prueba1.hidratos)
-    end
+      it "Tiene un valor para las grasas saturadas" do
+        expect(11.0).to eq(prueba1.saturada)
+      end
     
-    it "Tiene un valor para la cantidad de azúcar " do
-      expect(56.8).to eq(prueba1.azucar)
-    end
+      it "Tiene un valor para la cantidad de hidratos de carbono" do
+        expect(57.6).to eq(prueba1.hidratos)
+      end
     
-    it "Tiene un valor para las proteínas " do 
-      expect(6.0).to eq(prueba1.proteinas)
-    end
+      it "Tiene un valor para la cantidad de azúcar " do
+        expect(56.8).to eq(prueba1.azucar)
+      end
     
-    it "Tiene un valor para la cantidad de sal" do
-      expect(0.11).to eq(prueba1.sal)
-    end
-  end 
+      it "Tiene un valor para las proteínas " do 
+        expect(6.0).to eq(prueba1.proteinas)
+      end
+    
+      it "Tiene un valor para la cantidad de sal" do
+        expect(0.11).to eq(prueba1.sal)
+      end
+    end 
   
   context "Funciones de la clase ValorEnergetico"do 
+    
+    prueba1 = ValorEnergetico.new("Nutella",31.6,11.0,57.6,56.8,6.0,0.11,25.0,8)
     
     it "Tiene un método para calcular el valor energético en kJ" do 
       prueba1.calc_val_energetico_KJ
